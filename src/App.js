390b88bdc731320737be0903387ee001bc6a625a
Ilg0rbek <87908082+Ilg0rbek@users.noreply.github.com>
2023-01-11 11:02:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { VectorMap } from '@south-paw/react-vector-maps';
 import { data } from './store'
 import { Map } from './appc'
-
-function App() {
+const App = () => {
   return (
     <Map>
       <VectorMap {...data} checkedLayers={['nz-auk']} currentLayers={['nz-wgn']} />
     </Map>
   );
 }
-
 export default App;
